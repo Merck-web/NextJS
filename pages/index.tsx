9a07e6f8a3856e93589168f28bce4ab719651381
Your Name <you@example.com>
2022-01-16 13:07:47 +0500

    Btn component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Button } from "../components";
 import { Htag } from "../components/Htag/Htag";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>Текст</Htag>
-      <Htag tag='h2'>Текст</Htag>
-      <Htag tag='h3'>Текст</Htag>
+      <Button appearance='primary' arrow='right'>
+        Кнопка
+      </Button>
+      <Button appearance='ghost' arrow='down'>
+        Кнопка
+      </Button>
     </>
   );
 }
