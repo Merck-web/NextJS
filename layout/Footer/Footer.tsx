792db82aea30e444d3af5cc6123219283cd47048
Your Name <you@example.com>
2022-01-22 12:00:09 +0500

    Верстка footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,14 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.scss";
+import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>Mrclll © 2021 - {format(new Date(), "yyyy")} Все права защищены</div>
+      <a href="#!" target="_blank">Пользовательское соглашение</a>
+      <a href="#!" target="_blank">Политика конфиденциальности</a>
+    </div>
+  );
 };
