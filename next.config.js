93ee97fcb1383c56757fd8f3d9aca286695243f5
Your Name <you@example.com>
2022-02-04 11:59:55 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,29 +1,10 @@
 module.exports = {
-	images: {
-		domains: ['courses-top.ru']
-	},
-	webpack(config, options) {
-		config.module.rules.push({
-			loader: '@svgr/webpack',
-			issuer: /\.[jt]sx?$/,
-			options: {
-				prettier: false,
-				svgo: true,
-				svgoConfig: {
-					plugins: [{
-						name: 'preset-default',
-						params: {
-							override: {
-								removeViewBox: false
-							}
-						}
-					}],
-				},
-				titleProp: true,
-			},
-			test: /\.svg$/,
-		});
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
 
-		return config;
-	},
+    return config;
+  }
 };
